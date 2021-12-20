7ca71a430ac5c4fde56897769b4b3559dfd724d7
wvan1230 <winstonvan@live.ca>
2021-12-19 22:35:45 -0500

    Change getters to property functions

--- bgone/api_key_list.py
+++ bgone/api_key_list.py
@@ -4,12 +4,12 @@ import requests
 
 class api_key_list():
     def __init__(self, api_keys: list, account_url: str) -> None:
-        self.account_url = account_url
-        self.api_keys = self.__initialize_keys(api_keys)
-        self.curr_key = self.__get_new_key()
+        self._account_url = account_url
+        self._api_keys = self.__initialize_keys(api_keys)
+        self._curr_key = self.__get_new_key()
 
     def __get_num_credits(self, key: str) -> typing.Union[int, None]:
-        """returns the number of api credits left for the given key
+        """Returns the number of api credits left for the given key
 
         Args:
             key (str): the api key
@@ -19,7 +19,7 @@ class api_key_list():
             int | None: number of api credits left or None if request failed
         """
         headers = {'X-Api-Key': key}
-        response = requests.get(self.account_url, headers=headers)
+        response = requests.get(self._account_url, headers=headers)
         num_credits = None
 
         if response.status_code == requests.codes.ok:
@@ -29,21 +29,21 @@ class api_key_list():
         return num_credits
 
     def __initialize_keys(self, api_keys: typing.List[str]) -> dict:
-        """[summary]
+        """Initialize the key rotation 
 
         Args:
-            api_keys (List[str]): [description]
+            api_keys (List[str]): List of API keys
 
         Returns:
             dict: [description]
         """
-        num_credits = [self.__get_num_credits(api_key) for api_key in api_keys]
-        keys = dict(zip(api_keys, num_credits))
+        keys = {}
 
-        # remove any failed tasks or zero-credit keys
-        for key, item in keys.items():
-            if item is None or item == 0:
-                keys.pop(key)
+        # keep sucessful keys (> 0 calls left or non-failed keys)
+        for api_key in api_keys:
+            num_credits = self.__get_num_credits(api_key)
+            if num_credits is not None or num_credits > 0:
+                keys[api_key] = num_credits
 
         return keys
 
@@ -55,35 +55,35 @@ class api_key_list():
             str | None: A new key or None if one cannot be found.
         """
         new_key = None
-        if len(self.api_keys.keys()) != 0:
-            new_key = list(self.api_keys.keys())[0]
+        if len(self._api_keys.keys()) != 0:
+            new_key = list(self._api_keys.keys())[0]
         return new_key
 
-    def get_total_credits(self) -> int:
+    @property
+    def total_credits(self) -> int:
         """returns the total number of credits left
 
         Returns:
             int: number of credits left
         """
-        return sum(self.api_keys.values())
+        return sum(self._api_keys.values())
 
-    def get_key(self) -> typing.Union[str, None]:
-        """returns the current key
-
-        Returns:
-            typing.Union[str, None]: [description]
+    @property
+    def curr_key(self) -> typing.Union[str, None]:
+        """Returns the current key in rotation. Returns None if there is no
+        available key.
         """
-        return self.curr_key
+        return self._curr_key
 
     def use_key(self) -> None:
-        """ decreases the current key's credit by 1 and replaces it with a new
-        key if credits for it run out
+        """Decreases the current key's credit by 1 and replaces it with a new
+        key if credits for it run out.
         """
         if self.get_total_credits() == 0:
             return
-        
-        self.api_keys[self.curr_key] -= 1
 
-        if self.api_keys[self.curr_key] == 0:
-            self.api_keys.pop(self.curr_key)
-            self.curr_key = self.__get_new_key()
+        self._api_keys[self._curr_key] -= 1
+
+        if self._api_keys[self._curr_key] == 0:
+            self._api_keys.pop(self._curr_key)
+            self._curr_key = self.__get_new_key()

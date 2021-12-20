3692bf0864ab85bd7e6f0823e7b16cc91fce2f80
wvan1230 <winstonvan@live.ca>
2021-12-20 13:49:46 -0500

    Formatting

--- bgone/config.py
+++ bgone/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-API_KEYS = os.getenv('REMOVE_BG_API_KEY').split(', ')
+TOKEN = os.environ['DISCORD_TOKEN']
+API_KEYS = os.environ['REMOVE_BG_API_KEY'].split(', ')
 MSG_HISTORY_LIMIT = 10
 
 API_URL = 'https://api.remove.bg/v1.0'

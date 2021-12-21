5da3d5e23357f752438b626ffe821fef574fca8d
wvan1230 <winstonvan@live.ca>
2021-12-20 21:55:43 -0500

    Bug fix

--- bgone/api_key_list.py
+++ bgone/api_key_list.py
@@ -9,14 +9,13 @@ class api_key_list():
         self._curr_key = self.__get_new_key()
 
     def __get_num_credits(self, key: str) -> typing.Union[int, None]:
-        """Returns the number of api credits left for the given key
+        """Returns the number of API credits left for the given key.
 
         Args:
-            key (str): the api key
-            sessions(ClientSession): an aiohttp session
+            key (str): The API key.
 
         Returns:
-            int | None: number of api credits left or None if request failed
+            int | None: Number of API credits left or None if request failed.
         """
         headers = {'X-Api-Key': key}
         response = requests.get(self._account_url, headers=headers)
@@ -29,13 +28,13 @@ class api_key_list():
         return num_credits
 
     def __initialize_keys(self, api_keys: typing.List[str]) -> dict:
-        """Initialize the key rotation 
+        """Initialize the key rotation structure.
 
         Args:
-            api_keys (List[str]): List of API keys
+            api_keys (List[str]): List of API keys.
 
         Returns:
-            dict: [description]
+            dict: The API key map of {API key=number of credits left}.
         """
         keys = {}
 
@@ -48,8 +47,8 @@ class api_key_list():
         return keys
 
     def __get_new_key(self) -> typing.Union[str, None]:
-        """returns a new key if possible. Returns None if there are no more keys
-        to use
+        """Returns a new key if possible. Returns None if there are no more keys
+        to use.
 
         Returns:
             str | None: A new key or None if one cannot be found.
@@ -61,10 +60,10 @@ class api_key_list():
 
     @property
     def total_credits(self) -> int:
-        """returns the total number of credits left
+        """Returns the total number of credits left.
 
         Returns:
-            int: number of credits left
+            int: Total number of credits left.
         """
         return sum(self._api_keys.values())
 
@@ -72,18 +71,18 @@ class api_key_list():
     def curr_key(self) -> typing.Union[str, None]:
         """Returns the current key in rotation. Returns None if there is no
         available key.
+
+        Returns:
+            str | None
         """
         return self._curr_key
 
     def use_key(self) -> None:
         """Decreases the current key's credit by 1 and replaces it with a new
-        key if credits for it run out.
+        key if credits for the current key run out.
         """
-        if self.total_credits == 0:
-            return
-
         self._api_keys[self._curr_key] -= 1
 
-        if self._api_keys[self._curr_key] == 0:
+        if self._api_keys[self._curr_key] <= 0:
             self._api_keys.pop(self._curr_key)
             self._curr_key = self.__get_new_key()
